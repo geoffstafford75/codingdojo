e0dc3c533d7ef4ff9db75824e72a8a4740d041a3
Geoff Stafford <geoffreystafford@gmail.com>
2021-11-13 14:51:39 -0800

    button clicker

---
+++ WebFundamentals/Javascript/JavaScriptInTheBrowser/ButtonClicker/script.js
@@ -0,0 +1,12 @@
+function hide(element){
+    element.remove();
+}
+
+function login(element) {
+    element.innerText = 'Logout';
+}
+
+function alertninja(element) {
+    element.innerText = "1 Likes";
+    alert("The Ninja was Liked");
+}
