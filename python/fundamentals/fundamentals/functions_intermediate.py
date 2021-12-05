73054f4d07129c296a6f22ec777e39dc473f5c6e
Geoff Stafford <geoffreystafford@gmail.com>
2021-12-04 18:47:32 -0800

    function_update

--- python/fundamentals/fundamentals/functions_intermediate.py
+++ python/fundamentals/fundamentals/functions_intermediate.py
@@ -58,7 +58,7 @@ print(z)
 # the function loops through each dictionary in the list and prints each key and the 
 # associated value. For example, given the following list:
 
-def iterateDictionary(some_list):
+def iterate_dictionary(some_list):
     for x in some_list:
         first_name = x['first_name']
         last_name = x['last_name']
@@ -70,7 +70,7 @@ students = [
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-iterateDictionary(students) 
+iterate_dictionary(students) 
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
 # first_name - Michael, last_name - Jordan
@@ -83,19 +83,19 @@ iterateDictionary(students)
 # dictionaries and a key name, the function prints the value stored in that key for 
 # each dictionary. 
 
-def iterateDictionary2(key_name, some_list):
+def iterate_dictionary2(key_name, some_list):
     for x in some_list:
         print(x[key_name])
 
-iterateDictionary2("first_name", students)
-iterateDictionary2("last_name", students)
+iterate_dictionary2("first_name", students)
+iterate_dictionary2("last_name", students)
 
 # 4. Iterate Through a Dictionary with List Values
 # Create a function printInfo(some_dict) that given a dictionary whose values are all lists, 
 # prints the name of each key along with the size of its list, and then prints the associated 
 # values within each key's list.
 
-def printInfo(some_dict):
+def print_info(some_dict):
     for x in some_dict:
         key_name = x.upper()
         count = len(some_dict[x])
@@ -107,6 +107,6 @@ dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-printInfo(dojo)
+print_info(dojo)
 
     
