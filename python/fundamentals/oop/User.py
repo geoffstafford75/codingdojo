099c309178aaafe0076823431b251d9879ea7504
Geoff Stafford <geoffreystafford@gmail.com>
2021-12-08 00:27:02 -0800

    cheking in da code

--- python/fundamentals/oop/User.py
+++ python/fundamentals/oop/User.py
@@ -3,7 +3,6 @@ class User:
         self.name = name
         self.email = email
         self.account_balance = 0
-
     def make_deposit(self, amount):
         self.account_balance += amount	
         
--- python/fundamentals/oop/users_bank_accounts.py
+++ python/fundamentals/oop/users_bank_accounts.py
@@ -2,18 +2,16 @@ class BankAccount:
     # class attribute
     bank_name = "First National Dojo"
     accounts = []
-    def __init__(self, account_name, int_rate, balance=0):
-        self.account_name = account_name
+    def __init__(self, int_rate, balance=0):
         self.int_rate = int_rate
         self.balance = balance
-        BankAccount.all_accounts.append(self)
+        BankAccount.accounts.append(self)
 
     def deposit(self, amount):
-        if self.account_type == type:
-            self.balance += amount
+        self.balance += amount
         return self
 
-    def withdraw(self, amount, account_name):
+    def withdraw(self, amount):
         if BankAccount.can_withdraw(self.balance,amount):
             self.balance -= amount
         else:
@@ -22,8 +20,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
-        return self
+        return f"{self.balance}"
 
     def yield_interest(self):
         if self.balance > 0:
@@ -54,23 +51,33 @@ class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account = BankAccount(.02,1000)
+        self.account = {
+            "Checking" : BankAccount(.02, 2000),
+            "Savings" : BankAccount(.05, 3000)
+        }
     
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
+    def make_deposit(self, name, amount):
+        print(name)
+        self.account[name].deposit(amount)
+        return self
         
-    def make_withdrawal(self,amount): 
-        self.account.withdraw(amount)
+    def make_withdrawal(self, name, amount): 
+        self.account[name].withdraw(amount)
+        return self
 
     def display_user_balance(self):
-        print(f"User: {self.name}, Balance: {self.account.display_account_info}")
+        print(f"User: {self.name}, Checking Balance: {self.account['Checking'].display_account_info()}")
+        print(f"User: {self.name}, Savings Balance: {self.account['Savings'].display_account_info()}")
+        return self
 
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
         self.display_user_balance()
         other_user.display_user_balance()
+        return self
 
 guido = User("Guido Sarduchi", "guido@python.com")
-
+guido.make_deposit("Checking",5000)
+guido.display_user_balance()
 
